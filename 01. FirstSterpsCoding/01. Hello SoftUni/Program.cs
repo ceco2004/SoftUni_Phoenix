45c6b8d47529dd9ad90a8aa7d00ab71deea259a5
ceco2004 <ceco2004@abv.bg>
2022-10-03 13:50:44 +0300

    added first folder

---
+++ 01. FirstSterpsCoding/01. Hello SoftUni/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01._Hello_SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SOftUni");
+            Console.WriteLine("This is my first programm.");
+        }
+    }
+}

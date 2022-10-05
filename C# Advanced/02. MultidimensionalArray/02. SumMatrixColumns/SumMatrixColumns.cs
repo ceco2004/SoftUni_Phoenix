ac95fc5b3f2c121976a517a53554d2520a0bea5e
ceco2004 <ceco2004@abv.bg>
2022-10-05 16:18:55 +0300

    Add new project

---
+++ C# Advanced/02. MultidimensionalArray/02. SumMatrixColumns/SumMatrixColumns.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._SumMatrixColumns
+{
+    class SumMatrixColumns
+    {
+        static void Main(string[] args)
+        {
+            int[] data = Console.ReadLine()
+                            .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                            .Select(x => int.Parse(x))
+                            .ToArray();
+            int r = data[0];
+            int c = data[1];
+
+            int[,] matrix = new int[r, c];
+
+            for (int i = 0; i < r; i++)
+            {
+                int[] elements = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(x => int.Parse(x))
+                                   .ToArray();
+                for (int j = 0; j < Math.Min(c, elements.Length); j++)
+                {
+                    matrix[i, j] = elements[j];
+                }
+            }
+
+            for(int i = 0; i < matrix.GetLength(1); i++)
+            {
+                int sum = 0;
+
+                for(int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    sum += matrix[j, i];
+                }
+                Console.WriteLine($"The sum of elements ot col {i+1} is: {sum}");
+            }
+        }
+    }
+}

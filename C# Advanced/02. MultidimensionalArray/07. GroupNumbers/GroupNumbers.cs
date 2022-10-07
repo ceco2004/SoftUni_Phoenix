fde282c80b1f5cbe9c436b42113deddb7b1cba7a
ceco2004 <ceco2004@abv.bg>
2022-10-07 21:39:08 +0300

    Add project

---
+++ C# Advanced/02. MultidimensionalArray/07. GroupNumbers/GroupNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._GroupNumbers
+{
+    class GroupNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                            .Select(x => int.Parse(x))
+                            .ToArray();
+
+            int[][] groupMatrix = new int[3][];
+
+            for(int i = 0; i <= 2; i++)
+            {
+                groupMatrix[i] = numbers.Where(x => Math.Abs(x % 3) == i).ToArray();
+            }
+
+            foreach(int[] row in groupMatrix)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+    }
+}

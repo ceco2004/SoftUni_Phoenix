d2eb098156b5cc8fe57853282742e0c62ce4023a
ceco2004 <ceco2004@abv.bg>
2022-10-10 19:07:32 +0300

    Add project

---
+++ C# Advanced/03. SetsAndDictionaryAdvanced/01. CountSomeValuesInArray/CountSomeValuesInArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._CountSomeValuesInArray
+{
+    class CountSomeValuesInArray
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                         .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                         .Select(x => int.Parse(x))
+                         .ToArray();
+            Dictionary<int, int> numCounter = new Dictionary<int, int>();
+
+            foreach(int num in nums)
+            {
+                if(numCounter.ContainsKey(num) == false)
+                {
+                    numCounter[num] = 0;
+                }
+
+                numCounter[num]++;
+            }
+
+            foreach(var kvp in numCounter)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}

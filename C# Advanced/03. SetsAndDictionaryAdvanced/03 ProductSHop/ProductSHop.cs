3e72a8c2b6b4f574832cb859e42c9dcaf9348834
ceco2004 <ceco2004@abv.bg>
2022-10-10 19:45:38 +0300

    Add project

---
+++ C# Advanced/03. SetsAndDictionaryAdvanced/03 ProductSHop/ProductSHop.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03_ProductSHop
+{
+    class ProductSHop
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, Dictionary<string, double>> shopProductPrice = new SortedDictionary<string, Dictionary<string, double>>();
+
+            string input = Console.ReadLine();
+
+            while(input != "Revision")
+            {
+                string[] data = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string shop = data[0];
+                string product = data[1];
+                double price = double.Parse(data[2]);
+
+                if(shopProductPrice.ContainsKey(shop) == false)
+                {
+                    shopProductPrice[shop] = new Dictionary<string, double>();
+                }
+
+                shopProductPrice[shop][product] = price;
+
+                input = Console.ReadLine();
+            }
+
+            foreach(var shop in shopProductPrice)
+            {
+                Console.WriteLine($"{shop.Key}->");
+                foreach(var product in shop.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value:f2}");
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/03. SetsAndDictionaryAdvanced/04. CitiesByContinentAndCountryes/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._CitiesByContinentAndCountryes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, List<string>>> continentCountryCities
+                = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            FilltTheData(n, continentCountryCities);
+
+            Print(continentCountryCities);
+
+        }
+
+        private static void Print(Dictionary<string, Dictionary<string, List<string>>> continentCountryCities)
+        {
+            foreach(var continent in continentCountryCities)
+            {
+                Console.WriteLine($"{continent.Key}:");
+                foreach(var country in continent.Value)
+                {
+                    Console.WriteLine($"  {country.Key} -> {string.Join(", ", country.Value)}");
+                }
+            }
+        }
+
+        private static void FilltTheData(int n, Dictionary<string, Dictionary<string, List<string>>> continentCountryCities)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine()
+                                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string continent = data[0];
+                string country = data[1];
+                string city = data[2];
+
+                if (continentCountryCities.ContainsKey(continent) == false)
+                {
+                    continentCountryCities[continent] = new Dictionary<string, List<string>>();
+                }
+
+                if (continentCountryCities[continent].ContainsKey(country) == false)
+                {
+                    continentCountryCities[continent][country] = new List<string>();
+                }
+
+                continentCountryCities[continent][country].Add(city);
+            }
+        }
+    }
+}

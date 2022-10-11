1c8049a91dafbabe0693a8def8dafcc8e352cd0f
ceco2004 <ceco2004@abv.bg>
2022-10-11 21:03:48 +0300

    Add project

---
+++ C# Advanced/04. Streams/01. ReadFile/ReadFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace _01._ReadFile
+{
+    class ReadFile
+    {
+        static void Main(string[] args)
+        {
+            string path = "../../../";
+            string fileName = "ReadFile.cs";
+
+            path = Path.Combine(path, fileName);
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                int counter = 0;
+
+                string line = reader.ReadLine();
+
+                while(line != null)
+                {
+                    Console.WriteLine($"Line {++counter}: {line}");
+                    line = reader.ReadLine();
+                }
+                
+            }
+        }
+    }
+}

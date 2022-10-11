d354edfcde65455dc29833a19fddaff564b3a83c
ceco2004 <ceco2004@abv.bg>
2022-10-11 22:16:34 +0300

    Add project

---
+++ C# Advanced/04. Streams/03. WritingTextToFile/WritingTextToFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _03._WritingTextToFile
+{
+    class WritingTextToFile
+    {
+        static void Main(string[] args)
+        {
+            string text = "Всеки ден, малката мравка пристигаше рано на работното си място и започваше да работи.";
+            string path = Path.Combine("../../../output.txt");
+
+            FileStream stream = new FileStream(path, FileMode.Create);
+
+            try
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(text);
+                stream.Write(bytes, 0, bytes.Length);
+            }
+            finally
+            {
+                stream.Flush();
+            }
+        
+        }
+    }
+}
---
+++ C# Advanced/04. Streams/04. CopyFile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _04._CopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = Path.Combine("../../../ForAnt.txt");
+            string outputPath = Path.Combine("../../../copy.txt");
+
+            using (FileStream source = new FileStream(sourcePath, FileMode.Open))
+            {
+                using (FileStream destination = new FileStream(outputPath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[1024];
+                    int readBytes = source.Read(buffer, 0, buffer.Length);
+                    while(readBytes > 0)
+                    {
+                        destination.Write(buffer, 0, Math.Min(readBytes, buffer.Length));
+                        readBytes = source.Read(buffer, 0, buffer.Length);
+                    }
+                }
+            }
+        }
+    }
+}

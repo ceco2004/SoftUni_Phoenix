8da1e881475bba525202d0f33db29f3bcec48443
ceco2004 <ceco2004@abv.bg>
2022-10-11 21:20:33 +0300

    Add project

---
+++ C# Advanced/04. Streams/02. WriteToFile/WriteToFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _02._WriteToFile
+{
+    class WriteToFile
+    {
+        static void Main(string[] args)
+        {
+            string path = "../../..";
+            string fileSource = "WriteToFile.cs";
+            string fileOutput = "reverced.txt";
+
+            string sourcePath = Path.Combine(path, fileSource);
+            string outputPath = Path.Combine(path, fileOutput);
+
+            using(StreamReader reader = new StreamReader(sourcePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputPath))
+                {
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        writer.WriteLine(string.Join("", line.Reverse()));
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}

00e1b787249f4b7e20b44cb83edf264f2918f0fc
ceco2004 <ceco2004@abv.bg>
2022-10-14 21:14:37 +0300

    Add project

---
+++ C# OOP Basics/01. DefiningClassec/Ex01. DefiningClasses/DefiningClasses.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex01._DefiningClasses
+{
+    class DefiningClasses
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C# OOP Basics/01. DefiningClassec/Ex01. DefiningClasses/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex01._DefiningClasses
+{
+    public class Person
+    {
+        private int age;
+        private string name;
+
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+}

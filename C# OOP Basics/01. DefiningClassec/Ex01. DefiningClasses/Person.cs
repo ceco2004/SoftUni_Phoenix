eed6e3e62580c4631c722b118592e9e9272277f1
ceco2004 <ceco2004@abv.bg>
2022-10-14 21:22:24 +0300

    Add three constructors in the Person class

--- C# OOP Basics/01. DefiningClassec/Ex01. DefiningClasses/Person.cs
+++ C# OOP Basics/01. DefiningClassec/Ex01. DefiningClasses/Person.cs
@@ -11,5 +11,21 @@ namespace Ex01._DefiningClasses
 
         public int Age { get; set; }
         public string Name { get; set; }
+
+        public Person()
+        {
+            Age = 1;
+            Name = "No name";
+        }
+
+        public Person (int age): this()
+        {
+            Age = age;
+        }
+
+        public Person(string name, int age): this(age)
+        {
+            Name = name;
+        }
     }
 }

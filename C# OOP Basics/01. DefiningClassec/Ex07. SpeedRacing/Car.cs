2989ab53e23490e73880dca9bce0bf50dc69cde1
ceco2004 <ceco2004@abv.bg>
2022-10-15 13:49:44 +0300

    Add project

---
+++ C# OOP Basics/01. DefiningClassec/Ex07. SpeedRacing/Car.cs
@@ -0,0 +1,41 @@
+﻿namespace Ex07._SpeedRacing
+{
+    internal class Car
+    {
+        public string Model { get; private set; }
+        public double FuelAmount { get; private set; }
+        public double Consumption { get; private set; }
+        public double TraveledDistance { get; private set; }
+
+        internal Car(string model, double fuelAmount, double consumption)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            Consumption = consumption;
+        }
+
+        internal string MoveCar(int distanceToMove)
+        {
+            string msg = string.Empty;
+            if(distanceToMove * Consumption > FuelAmount)
+            {
+                msg = "Insufficient fuel for the drive";
+            }
+            else
+            {
+                FuelAmount -= distanceToMove * Consumption;
+                TraveledDistance += distanceToMove;
+
+                msg = $"{Model} was successful moving at {distanceToMove} kilometer/s";
+            }
+
+
+            return msg;
+        }
+
+        public override string ToString()
+        {
+            return $"Car: {Model}, have {FuelAmount:f2} liters fuel, and was moving {TraveledDistance} kilometers.";
+        }
+    }
+}
---
+++ C# OOP Basics/01. DefiningClassec/Ex07. SpeedRacing/SpeedRacing.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex07._SpeedRacing
+{
+    class SpeedRacing
+    {
+        static void Main(string[] args)
+        {
+            int carCount = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Car> cars = new Dictionary<string, Car>();
+
+            for(int i = 0; i < carCount; i++)
+            {
+                string[] data = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = data[0];
+
+                if (cars.ContainsKey(model))
+                {
+                    continue;
+                }
+
+                double fuelAmount = double.Parse(data[1]);
+                double conumption = double.Parse(data[2]);
+
+                Car car = new Car(model, fuelAmount, conumption);
+                cars[model] = car;
+            }
+
+            string input = Console.ReadLine();
+
+            while(input != "End")
+            {
+                string[] data = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string command = data[0];
+                
+                if(command == "Drive")
+                {
+                    string model = data[1];
+
+                    if (cars.ContainsKey(model))
+                    {
+                        int distanceToMove = int.Parse(data[2]);
+                        string msg = cars[model].MoveCar(distanceToMove);
+                        Console.WriteLine(msg);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach(var pair in cars)
+            {
+                Console.WriteLine(pair.Value);
+            }
+        }
+    }
+}

3add9447ec9b4fdc61979db5dc7f11e4a31b47ef
ceco2004 <ceco2004@abv.bg>
2022-10-16 12:45:44 +0300

    Add project

---
+++ C# Advanced/05. FunctionalProgramming/01. SortEvenNumbers/SortEvenNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._SortEvenNumbers
+{
+    class SortEvenNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                            .Select(n => int.Parse(n))
+                            .ToArray();
+
+            int[] evenSortedNumbers = numbers
+                                      .Where(n => n % 2 == 0)
+                                      .OrderBy(n => n)
+                                      .ToArray();
+
+            Console.WriteLine(string.Join(", ", evenSortedNumbers));
+        }
+    }
+}
---
+++ C# Advanced/05. FunctionalProgramming/02. SumNumbers/SumNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._SumNumbers
+{
+    class SumNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                            .Select(n => int.Parse(n))
+                            .ToArray();
+
+            string result = string.Empty;
+
+            numbers.Length.ToString().Select(r => result += r).ToArray();
+            result += Environment.NewLine;
+            numbers. Sum().ToString().Select(s => result += s).ToArray();
+
+            Console.WriteLine(result);
+        }
+    }
+}

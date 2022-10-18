c6ab49ea51e4307f7e6e5065381a84f01414ed85
ceco2004 <ceco2004@abv.bg>
2022-10-18 15:37:33 +0300

    Add project

---
+++ C# OOP Basics/04. Inheritance/01. SingleInheritance/Models/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01._SingleInheritance.Models
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+}
---
+++ C# OOP Basics/04. Inheritance/01. SingleInheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using _01._SingleInheritance.Models;
+using System;
+
+namespace _01._SingleInheritance
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+        }
+    }
+}

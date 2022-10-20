00f34059933f8ac8988cf2484bf89408a991da12
ceco2004 <ceco2004@abv.bg>
2022-10-20 15:04:56 +0300

    Add project Animals

---
+++ C# OOP Basics/04. Inheritance/Ex06. Animals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex06._Animals
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

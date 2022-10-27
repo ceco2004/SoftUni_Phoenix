4b865bdd4caa1f9eee466dfe467fd5956bd0f67a
ceco2004 <ceco2004@abv.bg>
2022-10-27 21:09:46 +0300

    Add project ArrayCreator

---
+++ C# OOP Advanced/02. Generics/02. GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,11 @@
+﻿namespace _02._GenericArrayCreator
+{
+    public static class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T item)
+        {
+            T[] arr = new T[length];
+            return arr;
+        }
+    }
+}
---
+++ C# OOP Advanced/02. Generics/02. GenericArrayCreator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02._GenericArrayCreator
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] nums = ArrayCreator.Create(10, 33);
+            var strings = ArrayCreator.Create(22, "Pesho");
+
+            Console.WriteLine(nums.GetType().Name);
+            Console.WriteLine(nums.Length);
+            Console.WriteLine(strings.GetType().Name);
+            Console.WriteLine(strings.Length);
+        }
+    }
+}

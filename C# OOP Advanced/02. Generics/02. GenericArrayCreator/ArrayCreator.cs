350f902d60953398859b0f055e6c758ddd34cc5d
ceco2004 <ceco2004@abv.bg>
2022-10-27 21:14:25 +0300

    Filling the array with given item

--- C# OOP Advanced/02. Generics/02. GenericArrayCreator/ArrayCreator.cs
+++ C# OOP Advanced/02. Generics/02. GenericArrayCreator/ArrayCreator.cs
@@ -5,6 +5,10 @@
         public static T[] Create<T>(int length, T item)
         {
             T[] arr = new T[length];
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = item;
+            }
             return arr;
         }
     }

9f904dcbb769c25aec7adcb218fc340e19a5eac0
ceco2004 <ceco2004@abv.bg>
2022-11-05 21:01:58 +0200

    Add project with reflextion run method's named as command

---
+++ C# OOP Advanced/04. ReflectionAndAttributes/P02_BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,32 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+            //TODO put your reflection code here
+
+            string command = Console.ReadLine();
+            Type type = typeof(BlackBoxInteger);
+            var constructor = type.GetConstructor(BindingFlags.Instance |BindingFlags.NonPublic, null,  new Type[] { typeof(int)}, null);
+            var instance = (BlackBoxInteger)constructor.Invoke(new object[] {0});
+
+            while (command != "END")
+            {
+                string[] args = command.Split("_", StringSplitOptions.RemoveEmptyEntries);
+                
+                var method = type.GetMethod(args[0], BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+                method.Invoke(instance, new object [] {int.Parse(args[1])});
+                var field = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic).First();
+                int value = (int)field.GetValue(instance);
+                Console.WriteLine(value);
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
